863feda50d8b0410c28c4a2f9285f86feb4b0fff
Gabi476 <gabriele.dutra@sptech.school>
2025-09-02 13:18:08 -0300

    ra:04251140-GabrieleDutra

--- src/main/java/school/sptech/Turma.java
+++ src/main/java/school/sptech/Turma.java
@@ -1,6 +1,71 @@
+
 package school.sptech;
 
-public class Turma {
+import org.apache.commons.lang3.IntegerRange;
 
-    
+import java.net.HttpRetryException;
+import java.sql.PreparedStatement;
+
+public class Turma {
+    private String nome;
+    private Double mensalidade;
+    private Integer qtdAluno;
+    private Boolean ativa;
+
+    public Turma(String nome, Double mensalidade){
+        this.nome=nome;
+        this.mensalidade=mensalidade;
+        qtdAluno=0;
+        ativa=true;
+    }
+
+    public void matricular(Integer qtd){
+        if(qtd!=null && qtd>0 && ativa==true){
+            qtdAluno=qtd;
+        }
+    }
+    public Integer cancelar(Integer qtd){
+        if(qtd!=null && qtd>0 && qtd<=qtdAluno && ativa==true){
+         qtdAluno-=qtd;
+         return qtdAluno;
+        }
+        return null;
+    }
+    public Integer desativar(){
+        if (ativa==true) {
+            ativa=false;
+            this.qtdAluno=0;
+            return qtdAluno;
+        }else{
+           return null;
+        }
+    }
+    public void transferir(Turma destino, Integer qtdATransferir){
+        if(ativa==true && destino!= null && qtdATransferir!=null && destino.ativa==true && qtdATransferir>0 && qtdATransferir<=qtdAluno){
+            qtdAluno-=qtdATransferir;
+            destino.qtdAluno+=qtdATransferir;
+        }
+    }
+    public Boolean reajustarMensalidade(Double reajuste){
+        if(reajuste!=null && reajuste>0){
+            mensalidade+=mensalidade*reajuste;
+            return true;
+        }
+        return false;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public Double getMensalidade(){
+        return mensalidade;
+    }
+    public Integer getQtdAluno(){
+        return qtdAluno;
+    }
+    public Boolean getAtiva(){
+        return ativa;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
 }
